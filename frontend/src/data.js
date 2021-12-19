540df4980b47fcb51514f61f0cd0d1b9a4bd31f9
gayathri1398 <gayathrivasu13@gmail.com>
2021-12-19 18:41:58 +0530

    added the data of products

---
+++ frontend/src/data.js
@@ -0,0 +1,76 @@
+const data ={
+    products:[
+        {
+            _id:1,
+            name:"Nike Slim Shirts",
+            category:"shirts",
+            image:"",
+            price: 120,
+            brand:"Nike",
+            countInStock:10,
+            description:"high quality product",
+            rating: 4.5,
+            numOfReviews:10
+        },
+        {
+            _id:2,
+            name:"Adidas Fit Shirt",
+            category:"shirts",
+            image:"",
+            price: 120,
+            brand:"Adidas",
+            countInStock:14,
+            description:"high quality product",
+            rating: 4.0,
+            numOfReviews:4
+        },
+        {
+            _id:3,
+            name:"Lacoste Free Shirts",
+            category:"shirts",
+            image:"",
+            price: 150,
+            brand:"Lacoste",
+            countInStock:0,
+            description:"high quality product",
+            rating: 4.2,
+            numOfReviews:6
+        },
+        {
+            _id:4,
+            name:"Nike Slim Pant",
+            category:"Pant",
+            image:"",
+            price: 160,
+            brand:"Nike",
+            countInStock:10,
+            description:"high quality product",
+            rating: 4.3,
+            numOfReviews:8
+        },
+        {
+            _id:5,
+            name:"Puma Slim Pant",
+            category:"Pant",
+            image:"",
+            price: 180,
+            brand:"Puma",
+            countInStock:17,
+            description:"high quality product",
+            rating: 3.8,
+            numOfReviews:5
+        },
+        {
+            _id:6,
+            name:"Adidas Fit Pant",
+            category:"Pant",
+            image:"",
+            price: 90,
+            brand:"Adidas",
+            countInStock:10,
+            description:"high quality product",
+            rating: 4.5,
+            numOfReviews:10
+        }
+    ]
+}
