f345d422b629c610e9c35f8b1eff120fcb8c0e03
gayathri1398 <gayathrivasu13@gmail.com>
2021-12-21 00:38:07 +0530

    styling the product page

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -2,42 +2,52 @@ import React from 'react';
 import Rating from 'react-rating-stars-component'
 
 const ProductPage=()=>{
+const ratingChanged=(newRating)=>{
+    console.log(newRating)
+}
+
     return<>
-       <div className='mt-8 w-full h-full flex flex-start justify-around'>
-           <div className=' w-1/3 h-screen ' >
+       <div className=' flex flex-col lg:flex-row mt-8 w-full h-full flex-start justify-around'>
+           <div className=' w-1/3 h-screen' >
                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwLAMgM3AoDdgJe289iPT2crK9qyjoauD73A&usqp=CAU" 
                alt="productname" 
                classname="w-full h-full object-fit"/>
            </div>
            <div>
-               <h1>Lacoste Slim shirt</h1>
-               <Rating/>
-               <span>1 reviews</span>
+               <h1 className='font-bold text-xl'>Lacoste Slim shirt</h1>
+               <Rating
+                  count={5}
+                  onChange={ratingChanged}
+                  size={24}
+                  activeColor="#ffd700"/>
+               <span className='text-indigo-600'>1 reviews</span>
                <p>Price:$120</p>
                <p>Description:This is a great product...</p>
            </div>
-           <div className='w-48 border-2 h-56 p-3 rounded-md'>
+
+           <div className='w-60 border-2 h-64 p-3 rounded-md'>
                <p>Seller</p>
-               <h2>Puma</h2>
-               <Rating/>
-               <span>120 reviews</span>
+               <h2 className='text-blue-900 font-bold'>Puma</h2>
+               <Rating
+                  count={5}
+                  onChange={ratingChanged}
+                  size={24}
+                  activeColor="#ffd700"/>
+               <span className='text-indigo-600'>120 reviews</span>
                <div className='flex justify-between'>
                    <p>Price</p>
                    <span>$60</span>
                </div>
                <div className='flex justify-between'>
                    <p>Status</p>
-                   <span className='text-green-500'>InStock</span>
+                   <span className='text-green-500'>InStock</span> 
                </div>
                <div className='flex justify-between'>
                    <p>Qty</p>
                    <span className='border-2 px-3'>1</span>
                </div>
-               
-               <p classname='border-2 border-black text-center'>Add to Cart</p>
-             
-               
-           </div>
+               <p className='border-2 border-gray-300 rounded-sm bg-yellow-300 mt-6 text-center'>Add to Cart</p>
+           </div>      
        </div>
     </>
 }
