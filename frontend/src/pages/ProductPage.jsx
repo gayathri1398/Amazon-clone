ddde316ba0dd01a97f2348fecd1ba255145e7f10
gayathri1398 <gayathrivasu13@gmail.com>
2021-12-21 19:06:06 +0530

    product page with the image

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -7,14 +7,14 @@ const ratingChanged=(newRating)=>{
 }
 
     return<>
-       <div className=' flex flex-col lg:flex-row mt-8 w-full h-full flex-start justify-around'>
-           <div className=' w-1/3 h-screen' >
-               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwLAMgM3AoDdgJe289iPT2crK9qyjoauD73A&usqp=CAU" 
+       <div className='flex flex-col gap-5 w-full h-full px-4 lg:flex-row mt-8 flex-start justify-around'>
+           <div className='lg:w-2/5' >
+               <img src="https://d2d8wwwkmhfcva.cloudfront.net/800x/d2lnr5mha7bycj.cloudfront.net/product-image/file/primary_fecfd990-3741-4913-9bf4-32f6d816782b.jpeg" 
                alt="productname" 
-               classname="w-full h-full object-fit"/>
+               classname='w-full h-full'/>
            </div>
-           <div>
-               <h1 className='font-bold text-xl'>Lacoste Slim shirt</h1>
+           <div className='lg:w-1/3'>
+               <h1 className='font-bold text-xl w-'>Lacoste Slim shirt</h1>
                <Rating
                   count={5}
                   onChange={ratingChanged}
@@ -23,8 +23,8 @@ const ratingChanged=(newRating)=>{
                <span className='text-indigo-600'>1 reviews</span>
                <p>Price:$120</p>
                <p>Description:This is a great product...</p>
-           </div>
-
+           
+               </div>  
            <div className='w-60 border-2 h-64 p-3 rounded-md'>
                <p>Seller</p>
                <h2 className='text-blue-900 font-bold'>Puma</h2>
@@ -47,7 +47,8 @@ const ratingChanged=(newRating)=>{
                    <span className='border-2 px-3'>1</span>
                </div>
                <p className='border-2 border-gray-300 rounded-sm bg-yellow-300 mt-6 text-center'>Add to Cart</p>
-           </div>      
+           </div> 
+             
        </div>
     </>
 }
