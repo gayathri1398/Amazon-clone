1fcdd995fff07b424775ed6aeacc651c1ac92868
gayathri1398 <gayathrivasu13@gmail.com>
2022-01-05 17:15:42 +0530

    added the cart page

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 const CartPage=()=>{
     const {_id}= useParams();
+    const {search}=useLocation();
+    const qtyInUrl=new URLSearchParams(search).get('qty');
+    const qty=qtyInUrl?Number(qtyInUrl):1
 
     return<>
     <div>
         <h1>CartPage</h1>
-        <p>ProductID:{_id} Quantity:{}</p>
+        <p>ProductID:{_id} Quantity:{qty}</p>
     </div>
     
     </>
